2ecb934bc0af35be6f9bed3fbc5e9a496fc8ae46
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2023-08-11 14:39:31 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv) - 1
     if (argc == 0):
-        print(argc, "argument.")
+        print(argc, "arguments.")
     elif (argc == 1):
         print(argc, "argument:".format(sys.argv[0]))
         print("1:", sys.argv[1])
